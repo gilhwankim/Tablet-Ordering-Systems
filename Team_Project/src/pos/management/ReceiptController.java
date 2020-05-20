5b2902b915fc2212d77de25a73b93ca5bb74377c
ryudain <65375802+ryudain@users.noreply.github.com>
2020-05-20 15:36:32 +0900

    영수증관리

---
+++ Team_Project/src/pos/management/ReceiptController.java
@@ -0,0 +1,15 @@
+package pos.management;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ReceiptController implements Initializable{
+
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+    
+   }
+      
+}
