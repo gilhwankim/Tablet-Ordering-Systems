e361194041c6ca82fdfe7b6b090dd97936234819
326gyun <65344678+326gyun@users.noreply.github.com>
2020-05-21 11:37:13 +0900

    영수증 거래일자 메서드 추가

--- Team_Project/src/pos/management/ReceiptController.java
+++ Team_Project/src/pos/management/ReceiptController.java
@@ -1,15 +1,29 @@
 package pos.management;
 
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class ReceiptController implements Initializable{
+	@FXML private Label currentDate; //거래일자 라벨
 
    @Override
-   public void initialize(URL location, ResourceBundle resources) {
+   public void initialize(URL location, ResourceBundle resources) {	   
+	   currentDateSetting(currentDate); 
+	   
+	   
     
    }
+   //거래일자 세팅해주는 메서드
+   public void currentDateSetting(Label currentDateLabel) {
+	   SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	   currentDateLabel.setText(sdf.format(new Date()));
+   }
+
       
 }
