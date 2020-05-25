e95f26dcd314b65e43f1c56d81fe48cfd45dccad
326gyun <65344678+326gyun@users.noreply.github.com>
2020-05-25 11:46:53 +0900

    벨소리 추가

--- Team_Project/src/makeSound/MakeSound.java
+++ Team_Project/src/makeSound/MakeSound.java
@@ -8,7 +8,7 @@ public class MakeSound {
    
    public static void kitchenOrderSound() {
       try {
-         FileInputStream   fis = new FileInputStream("src\\makeSound\\notice.mp3"); //음원 파일 경로
+         FileInputStream   fis = new FileInputStream("src\\makeSound\\bell.mp3"); //음원 파일 경로
          Player player = new Player(fis);
          player.play(); //플레이어 실행
       }catch (Exception e) {}
